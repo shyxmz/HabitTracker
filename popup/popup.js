d45bc4510812f4d25383f05d0176af74aa382a4e
Shyam Modi <shyammodi1729@gmail.com>
2024-11-05 23:18:31 +0530

    1.0022

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,14 @@
-// To load the data when the popup is opened //
-document.addEventListener('DOMContentLoaded',loadPopupData);
+// Load the data when the popup is opened
+document.addEventListener('DOMContentLoaded', loadPopupData);
 
-// Function to convert the time hours and minutes //
-function formatTime(minutes){
-    const hours = Math.floor(minutes/60);
-    const mins = minutes%60;
-    return `$(hours)h $(mins)m`;
+// Function to convert time to hours and minutes
+function formatTime(minutes) {
+    const hrs = Math.floor(minutes / 60);
+    const mins = minutes % 60;
+    return `${hrs}h ${mins}m`;
 }
 
-// To load and Display the usgae stats, progress, and time categories // 
+// Load and display the usage stats, progress, and time categories
 async function loadPopupData() {
     const data = await chrome.storage.local.get(['productiveTime', 'nonProductiveTime', 'goalProgress']);
     document.getElementById('productive-time').querySelector('span').textContent = formatTime(data.productiveTime || 0);
@@ -16,17 +16,17 @@ async function loadPopupData() {
     document.getElementById('usage-progress').value = data.goalProgress || 0;
 }
 
-// Open the dashboard page // 
+// Open the dashboard page
 function openDashboard() {
     chrome.tabs.create({url: chrome.runtime.getURL('dashboard/dashboard.html')});
 }
 
-// Open the settings page // 
-function openSettings(){
+// Open the settings page
+function openSettings() {
     chrome.runtime.openOptionsPage();
 }
 
-// Reset the stats and show notifications // 
+// Reset the stats and show notifications
 async function resetStats() {
     await chrome.storage.local.set({
         productiveTime: 0,
@@ -37,15 +37,15 @@ async function resetStats() {
     showNotifications("Statistics have been reset!");
 }
 
-// Display the notification (temporary) //
+// Display a temporary notification
 function showNotifications(message) {
     const notification = document.getElementById('notification-message');
     notification.textContent = message;
     notification.style.display = 'block';
-    setTimeout(() => notification.style.display = 'none',3000); // revert back after showing the notification for 3s // 
+    setTimeout(() => notification.style.display = 'none', 3000); // Hide after 3 seconds
 }
 
-// Event listeners for the buttons // 
+// Event listeners for the buttons
 document.getElementById('view-dashboard').addEventListener('click', openDashboard);
 document.getElementById('open-settings').addEventListener('click', openSettings);
-document.getElementById('reset').addEventListener('click',resetStats);
+document.getElementById('reset').addEventListener('click', resetStats);
